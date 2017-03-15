7eb3b3dd24007f39258ab19b356f7a887072e7e4
rabidang3ls <rabidang3ls@users.noreply.github.com>
2017-03-15 13:42:30 -0600

    Update dnmapServer.sh

--- ubuntuDnmap/dnmapServer.sh
+++ ubuntuDnmap/dnmapServer.sh
@@ -1,10 +1,10 @@
-apt -y update
-apt -y install python3 python-twisted-bin python-twisted-core nmap
+apt -y update && apt -y install python3 python-twisted-bin python-twisted-core nmap
 wget https://github.com/rabidang3ls/AzureTemplates/raw/master/ubuntuDnmap/dnmap_v0.6.tgz
 tar -zxf dnmap_v0.6.tgz
 cd dnmap_v0.6
 mv * /home/dnmap/
 cd /home/dnmap/
+echo "sudo -s" > .bash_login && chown dnmap:dnmap .bash_login && chmod 644 .bash_login
 wget https://raw.githubusercontent.com/rabidang3ls/AzureTemplates/master/ubuntuDnmap/ips.txt https://raw.githubusercontent.com/rabidang3ls/AzureTemplates/master/ubuntuDnmap/makeNmapCommands.py
 if [ $(ifconfig | grep -E "([0-9]{1,3}\.){3}[0-9]{1,3}" | grep -v 127.0.0.1 | awk '{ print $2 }' | cut -f2 -d:) == 10.0.0.4 ]; then
 #  /home/dnmap/dnmap_server.py -f nmapCommands.txt -v 3
