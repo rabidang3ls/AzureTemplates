09425b5cb668387a0602c96520643cfe0fa7bde1
rabidang3ls <rabidang3ls@users.noreply.github.com>
2017-03-16 10:38:33 -0600

    Create azureDnmap.py

---
+++ azureDnmap.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+
+import netifaces
+from os import system
+from argparse import ArgumentParser
+
+def main():
+
+	parser = ArgumentParser(formatter_class=RawTextHelpFormatter)
+	parser.add_argument('-f', '--file',
+		default='ips.txt',
+		dest='file',
+		help='The file containing IP addresses in one of three forms: \n    1. CIDR notation - 10.1.1.0/24\n    2. Range of IPs - 10.1.1.24-65\n    3. Individual IP - 10.1.1.4\n\n')
+	parser.add_argument('-fa', '--fast-args',
+		default='-Pn --top-ports 100',
+		dest='fast',
+		help='The arguments to be used in the initial fast scan. Use quotes to contain all arguments.')
+	# parser.add_argument('-ia', '--intermediate-args',
+	# 	default='',
+	# 	dest='intermediate',
+	# 	help='The arguments to be used in the intermediate scan.')
+	parser.add_argument('-ta', '--thorough-args',
+		default='-p 0-65535',
+		dest='thorough',
+		help='The arguments to be used in the final thorough scan. Use quotes to contain all arguments.')
+	args = parser.parse_args()
+
+	system('apt -y update && apt -y install python3 python-twisted-bin python-twisted-core nmap python-netifaces')
+	system('wget https://github.com/rabidang3ls/AzureTemplates/raw/master/ubuntuDnmap/dnmap_v0.6.tgz')
+	system('tar -zxf dnmap_v0.6.tgz')
+	system('cd dnmap_v0.6')
+	system('mv * /home/dnmap/')
+	system('cd /home/dnmap/')
+	system('echo \"sudo -s\" > .bash_login && chown dnmap:dnmap .bash_login && chmod 644 .bash_login')
+	system('wget https://raw.githubusercontent.com/rabidang3ls/AzureTemplates/master/ubuntuDnmap/ips.txt https://raw.githubusercontent.com/rabidang3ls/AzureTemplates/master/ubuntuDnmap/makeNmapCommands.py')
+
+	netifaces.ifaddresses('eth0')
+	ip = netifaces.ifaddresses('eth0')[2][0]['addr']
+	if ip == '10.0.0.4':
+		makeNmapCommands(args)
+		system('screen -S dnmap -d -m /home/dnmap/dnmap_server.py -f nmapCommands.txt -v 3')
+
+	else:
+		system('screen -S dnmap -d -m /home/dnmap/dnmap_client.py -s 10.0.0.4 -d -a $(ifconfig | grep -E "([0-9]{1,3}\.){3}[0-9]{1,3}" | grep -v 127.0.0.1 | awk \'{ print $2 }\' | cut -f2 -d:)')
+
+def makeNmapCommands(args):
+
+	with open(args.file, 'r') as f:
+		iplist = f.read().splitlines()
+
+	for item in iplist:
+		item2 = ''
+
+		if '/' in item:
+			item2 = item.replace('/', 'slash')
+
+		if not item2:
+			jobsFast.append('nmap ' + args.fast + ' -oA {0}.fast {0}'.format(item))
+			# jobsIntermediate.append('nmap command')
+			jobsThorough.append('nmap ' + args.thorough + ' -oA {0}.thorough {0}'.format(item))
+		else:
+			jobsFast.append('nmap ' + args.fast + ' -oA {0}.fast {1}'.format(item2,item)) #add -Pn?
+			# jobsIntermediate.append('nmap command')
+			jobsThorough.append('nmap ' + args.thorough + ' -oA {0}.thorough {1}'.format(item2,item))
+
+	with open('nmapCommands.txt', 'w') as f:
+
+		for item in jobsFast:
+			f.write(item + '\n')
+
+		for item in jobsThorough:
+			f.write(item + '\n')
+
+
+if __name__ == '__main__':
+	main()
+
